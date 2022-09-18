23f5fa0f086546927fd4fe8f7fb366fe4a24a855
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2022-09-18 19:32:28 +0545

    Update

--- sample/src/component/common/home/product_list/index.js
+++ sample/src/component/common/home/product_list/index.js
@@ -1,42 +1,42 @@
 import { useState } from "react"
-import { Col, Container,Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import SingleProductComponent from "../single-product";
 
 
-export const ProductListComponent =({product})=>{
-    let [allProducts , setAllProducts]= useState();
+export const ProductListComponent = ({ product }) => {
+    let [allProducts, setAllProducts] = useState();
     return (
         <>
-        
-        <Row className="mt-3 brand-title">
-            <Col>
-                <h1 className="text-center">
-                   All Product List
-                </h1>
-            </Col>
-        </Row>
-        <Container fluid>
-            <Row>
-                <Col sm={6} md={3} className="mb-4" >
-                    <SingleProductComponent/>
-                </Col>
-                <Col sm={6} md={3} className="mb-4" >
-                    <SingleProductComponent/>
-                </Col>
-                <Col sm={6} md={3} className="mb-4" >
-                    <SingleProductComponent/>
-                </Col>
-                <Col sm={6} md={3} className="mb-4" >
-                    <SingleProductComponent/>
-                </Col>
-                <Col sm={6} md={3} className="mb-4" >
-                    <SingleProductComponent/>
-                </Col>
-                <Col sm={6} md={3} className="mb-4" >
-                    <SingleProductComponent/>
+
+            <Row className="mt-3 brand-title">
+                <Col>
+                    <h1 className="text-center">
+                        All Product List
+                    </h1>
                 </Col>
             </Row>
-        </Container>
+            <Container fluid>
+                <Row>
+                    <Col sm={6} md={3} className="mb-4" >
+                        <SingleProductComponent />
+                    </Col>
+                    <Col sm={6} md={3} className="mb-4" >
+                        <SingleProductComponent />
+                    </Col>
+                    <Col sm={6} md={3} className="mb-4" >
+                        <SingleProductComponent />
+                    </Col>
+                    <Col sm={6} md={3} className="mb-4" >
+                        <SingleProductComponent />
+                    </Col>
+                    <Col sm={6} md={3} className="mb-4" >
+                        <SingleProductComponent />
+                    </Col>
+                    <Col sm={6} md={3} className="mb-4" >
+                        <SingleProductComponent />
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
-    }
+}
