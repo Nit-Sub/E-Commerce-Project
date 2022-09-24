9ab34cb3c7832c3186e672b4b1acee2dc6118361
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2022-09-24 19:31:06 +0545

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log("HEllo")
+function reverseNumber() {
+
+    let digit = 12345;
+    let reverse = 0;
+    let n = 0
+    while (n < 5) {
+        digit = n % 10
+        reverse = reverse * 10 + digit
+    }
+    return reverse
+
+}
