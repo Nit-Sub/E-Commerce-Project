01c8a1e826c8544e76652ae2c35b50511fbc84cd
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2022-09-26 19:39:25 +0545

    Update test.js

--- test.js
+++ test.js
@@ -1,36 +1,48 @@
-const firstPromise = () => {
-    return new Promise((res, rej) => {
-        res("I  am first promise")
-    })
+// const firstPromise = () => {
+//     return new Promise((res, rej) => {
+//         res("I  am first promise")
+//     })
 
+// }
+
+// const secondPromise = () => {
+//     return new Promise((res, rej) => {
+
+//         res("I am second Promise")
+//     })
+// }
+
+// const thirdPromise = () => {
+//     return new Promise((res, rej) => {
+
+//         res("I am third Promise") 
+//     })
+// }
+
+
+// firstPromise()
+//     .then(
+//         (res) => {
+//             console.log("First", res)
+
+//         }
+//     )
+//     .catch(
+//         (rej)=>{
+//             console.log("Rej:",rej)
+//         }
+
+
+//     )
+
+
+function User (){
+    this.name="";
+    this.rol="";
 }
 
-const secondPromise = () => {
-    return new Promise((res, rej) => {
-
-        res("I am second Promise")
-    })
-}
-
-const thirdPromise = () => {
-    return new Promise((res, rej) => {
-
-        res("I am third Promise")
-    })
-}
-
-
-firstPromise()
-    .then(
-        (res) => {
-            console.log("First", res)
-
-        }
-    )
-    .catch(
-        (rej)=>{
-            console.log("Rej:",rej)
-        }
-
-
-    )
+let user= new User();
+console.log(user)
+user.name="Nitesh Subedi";
+console.log(user)
+console.log(user.name)
