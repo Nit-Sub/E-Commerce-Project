22b17a7e5b7f2b4bb45625f6c3012017569188a0
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2022-09-26 18:57:39 +0545

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,36 @@
+const firstPromise = () => {
+    return new Promise((res, rej) => {
+        res("I  am first promise")
+    })
 
+}
+
+const secondPromise = () => {
+    return new Promise((res, rej) => {
+
+        res("I am second Promise")
+    })
+}
+
+const thirdPromise = () => {
+    return new Promise((res, rej) => {
+
+        res("I am third Promise")
+    })
+}
+
+
+firstPromise()
+    .then(
+        (res) => {
+            console.log("First", res)
+
+        }
+    )
+    .catch(
+        (rej)=>{
+            console.log("Rej:",rej)
+        }
+
+
+    )
