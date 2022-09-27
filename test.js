ea5d4671e534d63bc99bffa1974eb40ddb89385a
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2022-09-27 15:55:17 +0545

    Update test.js

--- test.js
+++ test.js
@@ -47,26 +47,26 @@
 // console.log(user)
 // console.log(user.name)  
 
-class Product 
-{
-    name;
-    price;
-    discount;   
-    brand;
-    constructor (data){
-        this.name= data.name;
-        this.price=data.price;
-        this.discount=data.discount;
-        this.brand=data.brand;
-    }
-    afterDiscount= function(){
-        let afterDiscount = this.price - this.price*this.discount/100;
-        this.afterDiscount=afterDiscount;
-    }
-}
-let Product_item = new Product({
-    name:"Iphone" , price :"100000", discount:10 , brand:"Test"
-});
-console.log(Product_item)
-Product_item.afterDiscount();
-console.log(Product_item)
+// class Product 
+// {
+//     name;
+//     price;
+//     discount;   
+//     brand;
+//     constructor (data){
+//         this.name= data.name;
+//         this.price=data.price;
+//         this.discount=data.discount;
+//         this.brand=data.brand;
+//     }
+//     afterDiscount= function(){
+//         let afterDiscount = this.price - this.price*this.discount/100;
+//         this.afterDiscount=afterDiscount;
+//     }
+// }
+// let Product_item = new Product({
+//     name:"Iphone" , price :"100000", discount:10 , brand:"Test"
+// });
+// console.log(Product_item)
+// Product_item.afterDiscount();
+// console.log(Product_item)
