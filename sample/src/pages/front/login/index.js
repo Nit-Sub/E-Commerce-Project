a2b19608ef1b73407181bdeb1120731cb622a89d
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2022-12-06 20:50:14 +0545

    Update index.js

--- sample/src/pages/front/login/index.js
+++ sample/src/pages/front/login/index.js
@@ -1,5 +1,5 @@
-import React,{ useState, useEffect } from "react";
-import { Container, Row, Col, Form, Button} from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { NavLink, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { loginSvc } from "../../../services/auth.service";
@@ -13,10 +13,10 @@ const LoginPage = () => {
     email: '',
     password: ''
   })
-  
-  let navigate=useNavigate();
+
+  let navigate = useNavigate();
   const handleChange = (e) => {
-    let {value, name} = e.target;
+    let { value, name } = e.target;
     setData(
       {
         ...data,
@@ -33,37 +33,37 @@ const LoginPage = () => {
         msg = value ? "" : "Username is required";
         break;
       case "password":
-        msg = value ? (value.length<8 ? 'Password need to be 8 character long' : "") : "Password is required";
+        msg = value ? (value.length < 8 ? 'Password need to be 8 character long' : "") : "Password is required";
         break;
       // no default
-      default: 
+      default:
         break;
-  }
-    setErr ({
+    }
+    setErr({
       ...err,
-      [field]:msg
+      [field]: msg
     })
 
   }
-  const handleSubmit=async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    let result= await loginSvc(data.email, data.password);
-    if (result){
+
+    let result = await loginSvc(data.email, data.password);
+    if (result) {
       toast.success("welcome to cms portal")
-      navigate("/"+result.user.role)
+      navigate("/" + result.user.role)
     }
     // TODO: API call garney yesma
-    
+
   }
-  useEffect(()=>{
-    let token= localStorage.getItem('token');
-    let logged_in_user=JSON.parse(localStorage.getItem('loggedInUser'));
-    if (token & logged_in_user){
-      navigate("/"+ logged_in_user['role'])
+  useEffect(() => {
+    let token = localStorage.getItem('token');
+    let logged_in_user = JSON.parse(localStorage.getItem('loggedInUser'));
+    if (token & logged_in_user) {
+      navigate("/" + logged_in_user['role'])
     }
-  },[navigate]); 
-  
+  }, [navigate]);
+
   return (
     <>
       <Container>
@@ -88,7 +88,7 @@ const LoginPage = () => {
                     onChange={handleChange}
                   />
                   {
-                    err && err.username&&
+                    err && err.username &&
                     <span className="text-danger">{err.username}</span>
                   }
                 </Col>
@@ -105,7 +105,7 @@ const LoginPage = () => {
                     placeholder="Please Enter Your Password....."
                     onChange={handleChange}
                   />
-                   {
+                  {
                     err && err.password &&
                     <span className="text-danger">{err.username}</span>
                   }
@@ -129,7 +129,7 @@ const LoginPage = () => {
 
             </Form>
             <p>Or</p>
-            <NavLink className='btn-link' to ="/register">Register</NavLink>
+            <NavLink className='btn-link' to="/register">Register</NavLink>
           </Col>
         </Row>
       </Container>
